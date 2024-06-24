22197babd0b01b41a3c8e82a8f6f91dae4eb1c41
Drew Zarn <drew@abmp.com>
2024-06-24 13:05:32 -0600

    Very basicest setup

---
+++ config.example.js
@@ -0,0 +1,4 @@
+let CONFIG = {
+    HA_URL: 'https://ha-server.com/api/',
+    HA_TOKEN: 'ABC123'
+}
---
+++ wx.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    if(window.CONFIG == undefined) {
+        $('#main').text("CONFIG is not defined. Please check your config.js file.");
+        return;
+    }
+});
