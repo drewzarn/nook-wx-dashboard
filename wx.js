2863dbe4061d7506b8a6ccf36c06d627a0ee2c27
Drew Zarn <drew@abmp.com>
2024-06-24 13:12:52 -0600

    Get token from URL

--- wx.js
+++ wx.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         $('#main').text("CONFIG is not defined. Please check your config.js file.");
         return;
     }
+    if(window.CONFIG.HA_TOKEN == 'URL') {
+        const searchParams = new URLSearchParams(window.location.search);
+        window.CONFIG.HA_TOKEN = searchParams.get('t');
+    }
 });
